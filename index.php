27cecce8b251de338341da9d1ba10f60332afa43
Sarvarovalmaz <47254886+Sarvarovalmaz@users.noreply.github.com>
2019-10-01 11:26:29 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+function n1()
+{
+    $a = 3;
+    $b = a;
+    return b;
+}
+echo n1();
+//
+function n2 ()
+{
+    $a = 10;
+    $b = 2;
+    $c = (a + b) / 2;
+    $o = (a - b) / 2;
+    $u = (a * b) / 2;
+    $p = (a / b) / 2;
+    return c, o , u, p;
+}
+echo n2;
+function n3 (){
+$c = 15;
+$d = 2;
+$result = c + d;
+}
+echo $result;
+function n4 ()
+{
+    $a = 10;
+    $b = 2;
+    $c = 5;
+    $q = (a + b + c) / 3;
+    return q;
+}
+echo n4;
+function n5 (){
+$a = 17;
+$b = 10;
+$c = a - b;
+$d = 17;
+$result = c + d;
+return $result;
+}
+echo n5;
+function n6 ()
+{
+    $a = "привет мир";
+    $b = "моя первая программа";
+    $c = a + b;
+    return c;
+}
+//
+function n7 (){
+    $text1 ="Привет";
+    $text2 = "Мир";
+    $text3 = text1 + text2;
+    return text3;
+}
+echo n7;
+function n8 (){
+    $name = 'Алмаз';
+    return 'Привет'.$name;
+}
+echo n8;
+function n9 (){
+    $text = 'abcde';
+    echo $text[0];
+    echo $text[2];
+    echo $text[4];
+    return $text;
+}
+echo n9;
+function n10 (){
+    $text = 'abcde';
+    $text[0] = '!';
+    echo $text;
+    return $text;
+
+}echo n10;
+
+
+function n11 () {
+
+    $num = '12345';
+    echo $num[0]+$num[1]+$num[2]+$num[3]+$num[4];
+    return num;
+}echo n11;
+
+
+
+
+
+
